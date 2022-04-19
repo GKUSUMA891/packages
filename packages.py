f81eefffb03aabc5660de7b06744d8f33d965d7f
kusuma <kusuma.g@thundersoft.com>
2022-04-19 01:44:16 -0700

    package file

---
+++ packages.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    print("Sum : ", x+y)
+    return
+
+def mul(x,y):
+    print("Product : ", x*y)
+    return
+
+def sub(x,y):
+    print("Difference : ", x-y)
+    return
+def div(x,y):
+    print("Division : ", x/y)
+    return
+
+def test_calc():
+    add(10,20)
+    mul(30,5)
+    sub(450,100)
+    div(20,3)
+    return
+
+test_calc()
