ecf9e04a2b5a814c3843c85c6d23efede2c7b938
kusuma <kusuma.g@thundersoft.com>
2022-04-19 01:53:43 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import arthmatic_op as ao
+import add as ad
+import multiplication as ml
+import substrction as sb
+import division as dv
+def test_calc():
+    ad.add(10,20)
+    ml.mul(20,3)
+    sb.sub(12,1)
+    dv.div(100,5)
+    return
+
+test_calc()
