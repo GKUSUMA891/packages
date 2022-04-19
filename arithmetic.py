cbb2406a9c2014813e28b1ff7482df32a77bb202
kusuma <kusuma.g@thundersoft.com>
2022-04-19 01:58:09 -0700

    create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print("Sum : ", x+y)
+    return
+
+def mul(x,y):
+    print("Product : ", x*y)
+    return
+
+def sub(x,y):
+    print("Difference : ", x-y)
+    return
+
+def div(x,y):
+    print("Division : ", x/y)
+    return
